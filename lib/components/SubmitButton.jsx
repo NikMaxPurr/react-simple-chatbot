af981b3adf5d66c6f64410fac94e9aa7c1e79396
Nikita Maximenko <maksimenko.nikita@purrweb.com>
2021-01-14 15:13:51 +0600

    Changed submit icon

--- lib/components/SubmitButton.jsx
+++ lib/components/SubmitButton.jsx
@@ -12,30 +12,17 @@ const fillFunc = props => {
 };
 
 const SubmitButton = styled.button`
-  background-color: transparent;
-  border: 0;
-  border-bottom-right-radius: 10px;
-  box-shadow: none;
-  cursor: ${props => (props.disabled ? 'default' : 'pointer')};
-  fill: ${fillFunc};
-  opacity: ${props => (props.disabled && !props.invalid ? '.5' : '1')};
-  outline: none;
-  padding: 14px 16px 12px 16px;
-  &:before {
-    content: '';
-    position: absolute;
-    width: 23px;
-    height: 23px;
-    border-radius: 50%;
-    animation: ${({ theme, speaking }) =>
-      speaking
-        ? css`
-            ${pulse(theme.headerBgColor)} 2s ease infinite
-          `
-        : ''};
-  }
-  &:not(:disabled):hover {
-    opacity: 0.7;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 28px;
+  height: 28px;
+  background: #e1dcfa;
+  border-radius: 50%;
+  transition: 0.3s ease-out;
+
+  &:hover {
+    background: #bfafe3;
   }
 `;
 
